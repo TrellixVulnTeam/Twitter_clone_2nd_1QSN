9a14909baaf5daaa7779e595cfd1d992c7730e5a
unmo0 <unmo001@gmaill.com>
2021-01-07 23:34:27 +0900

    save

--- accounts/models.py
+++ accounts/models.py
@@ -11,8 +11,7 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.CharField(max_length=248)
     created_data = models.DateTimeField(default=django.utils.timezone.now)
-    lat = models.DecimalField('緯度', max_digits=8, decimal_places=6)
-    lng = models.DecimalField('経度', max_digits=9, decimal_places=6)
+    like = models.IntegerField(default=0,null=True)
 
     def create(self):
         self.created_data = django.utils.timezone.now()
@@ -20,9 +19,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.post
-#
-# class Like(models.Model):
-#     article = models.ForeignKey(Post,on_delete=models.CASCADE)
-#     user = models.ForeignKey(User,on_delete=models.CASCADE)
-#     timestamp
+
+
 
